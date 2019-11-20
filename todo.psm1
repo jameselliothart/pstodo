456022983fe871703b696e1cd0197976376b8bd6
James Hart <james.elliot.hart@gmail.com>
2019-11-20 17:21:31 -0600

    version controlling todo module

---
+++ todo.psm1
@@ -0,0 +1,79 @@
+function todo {
+    Param(
+        [ValidateSet('a','r')]
+        [string] $AddRemove,
+        [string] $Item,
+        [string] $Path = 'H:\todo.txt'
+    )
+
+    if (!(Test-Path $Path)) {
+        New-Item $Path -ItemType File -Force | Out-Null
+        return "New todo file created in $Path"
+    }
+    [string[]] $todoItems = Get-Content $Path | Where-Object {$_} # ignore blank lines
+    $updatedTodo = @()
+
+    switch ($AddRemove) {
+        'a' {
+            $Item, $todoItems | ForEach-Object {$updatedTodo += $_}
+            $updatedTodo | Set-Content $Path
+            todo -Path $Path
+        }
+        'r' {
+            for ($i = 0; $i -lt $todoItems.Count; $i++) {
+                if ($i -ne $Item) {$updatedTodo += $todoItems[$i]}
+                else {New-TodoCompleted -Item $todoItems[$i] -Path (Get-DonePath $Path)}
+            }
+            if (!$updatedTodo) {Set-Content $Path -Value ""}
+            else {$updatedTodo | Set-Content $Path}
+            todo -Path $Path
+        }
+        default {
+            if (!$todoItems) {"No todos in $Path!"}
+            else {Write-TodoItems -Items $todoItems}
+        }
+    }
+}
+
+function Get-DonePath {
+    Param(
+        [Parameter(Mandatory)]
+        [string] $Path
+    )
+    $parent = $Path | Split-Path
+    $parts = ($Path | Split-Path -Leaf).Split('.')
+    $name = $parts[0..($parts.Count - 2)]
+    $doneName = ($name, 'done', $parts[-1] | %{$_}) -join '.'
+    Join-Path $parent $doneName
+}
+
+function Write-TodoItems {
+    Param(
+        [Parameter(Mandatory)]
+        [string[]] $Items
+    )
+    for ($i = 0; $i -lt $Items.Count; $i++) {
+        "$i. $($Items[$i])"
+    }
+}
+
+function New-TodoCompleted {
+    Param(
+        [string] $Item,
+        [string] $Path = 'H:\todo.done.txt'
+    )
+    $timestamp = Get-Date -Format 'yyyy-MM-dd HH:mm:ss'
+    $entry = "[$timestamp] $Item"
+    Add-Content -Value $entry -Path $Path
+}
+
+function done {
+    Param(
+        [int] $Tail = 10,
+        [string] $Path = 'H:\todo.done.txt'
+    )
+    if (!(Test-Path $Path)) {return "done file not found in '$Path'"}
+    Get-Content $Path -Tail $Tail
+}
+
+Export-ModuleMember -Function todo, done
---
+++ todo.tests.ps1
@@ -0,0 +1,41 @@
+Import-Module $PSScriptRoot\todo.psm1 -Force
+
+$testPath = "TestDrive:\todo.txt"
+
+$mockTodo = "first
+second
+another"
+
+InModuleScope todo { # lets us access the module's internal members
+Describe 'todo' {
+    Context 'Write-TodoItems' {
+        It 'should display indexed todo items' {
+            $items = $mockTodo.Split().Where({$_})
+            Write-TodoItems $items | Should -Be "0. first", "1. second", "2. another"
+        }
+    }
+    Context 'todo' {
+        BeforeEach {
+            Set-Content $testPath -Value $mockTodo -Force
+        }
+        It 'should display todo items when a/r not specified' {
+            $expected = "0. first", "1. second", "2. another"
+            todo -Path $testPath | Should -Be $expected
+        }
+        It 'should add an item when "a" specified with an item' {
+            $expected = "0. my item", "1. first", "2. second", "3. another"
+            todo a 'my item' -Path $testPath | Should -Be $expected
+        }
+        It 'should remove an item when "r" specified with an index' {
+            $expected = "0. first", "1. another"
+            todo r 1 -Path $testPath | Should -Be $expected
+        }
+        It 'should write a removed item to the done file' {
+            $donePath = Get-DonePath $testPath
+            New-Item $donePath -Force
+            todo r 1 -Path $testPath | Out-Null
+            Get-Content $donePath | Should -HaveCount 1
+        }
+    }
+}
+}
